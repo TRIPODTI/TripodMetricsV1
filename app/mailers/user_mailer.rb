dda2151aaa5b01da7b76b128e9c17c96c9300097
TRIPODTI <relacionescomerciales@tripod.net.co>
2012-10-08 16:05:26 -0500

    initial commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url  = "http://example.com/login"
     @device = #device
-    mail(:to => 'jorgecardenas@tripod.net.co',:from => 'relacionescomerciales@tripod.net.co', :subject=>"Notificación Tripod Metrics" )
+    mail(:to => 'jorgecardenas@tripod.net.co',:from => 'relacionescomerciales@tripod.net.co', :subject => "Notificacion Tripod Metrics" )
   end
 end
